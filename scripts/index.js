86c2927ca804be52ea80d5a310b25864bf9c58c6
koriahin-serhii <koriahin.s.work@gmail.com>
2025-03-31 11:26:57 +0200

    init project

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const toSecondPage = document.getElementById("toSecondPage");
+
+toSecondPage.addEventListener("click", () => {
+  window.location.pathname = "/page2.html";
+});
---
+++ scripts/index2.js
@@ -0,0 +1,4 @@
+const toMainPage = document.getElementById("toMainPage");
+toMainPage.addEventListener("click", () => {
+  location.pathname = "/index.html";
+});
